5a4d80786f40d51069daea7c2df8cc1a29915aa6
Lorenzo <lorenzodemedio@duck.com>
2024-11-30 11:41:58 +0100

    ex00 -> ex04 C00

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lde-medi <lde-medi@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 11:31:59 by lde-medi          #+#    #+#             */
+/*   Updated: 2024/11/30 11:37:11 by lde-medi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+
+/* int main()
+{
+	ft_print_reverse_alphabet();
+	return (0);
+} */
